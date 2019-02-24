97fec59181af2f7d34081e6d881249a7dd8caf76
Dave <david.szkuta@email.cz>
2019-02-24 20:33:34 +0100

    test VSCode

--- test.py
+++ test.py
@@ -1,5 +1,17 @@
+#%%
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 
 array = [5, 8, 9, 45 ,3, 8]
+array2 = [5,8, 9, 45, 3 ,8]
 
 for i, element in enumerate(array):
     print("This is i :", i, "This is element: ", element)
+
+#%%
+sns.distplot(array)
+plt.xlabel("Test")
+plt.show()
+
+#%%
